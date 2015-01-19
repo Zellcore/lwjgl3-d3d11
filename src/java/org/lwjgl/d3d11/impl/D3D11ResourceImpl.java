2f20a81ca40c7230486d663aa47f0bdc075b91c7
Kai Burjack <kburjack@googlemail.com>
2015-01-19 21:28:49 +0100

    ~ refactoring

--- src/java/org/lwjgl/d3d11/impl/D3D11ResourceImpl.java
+++ src/java/org/lwjgl/d3d11/impl/D3D11ResourceImpl.java
@@ -2,7 +2,7 @@ package org.lwjgl.d3d11.impl;
 
 import org.lwjgl.d3d11.ID3D11Resource;
 
-public class D3D11ResourceImpl extends D3DDeviceChildImpl implements ID3D11Resource {
+public class D3D11ResourceImpl extends D3D11DeviceChildImpl implements ID3D11Resource {
 
     public D3D11ResourceImpl(long ptr) {
         super(ptr);
